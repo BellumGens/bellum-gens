88daa2cfb78478b1f1762f9f3573eb62d2b5adea
Konstantin Dinev <kdinev@mail.bw.edu>
2018-08-20 18:32:54 +0300

    Updating the availability UI

--- src/app/player-details/player-details.component.ts
+++ src/app/player-details/player-details.component.ts
@@ -1,9 +1,9 @@
-import { Component, ViewEncapsulation, ViewChild } from '@angular/core';
+import { Component, ViewEncapsulation, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { LoginService } from '../services/login.service';
-import { SteamUser, SteamUserWithStats, DayOfWeek } from '../models/steamuser';
+import { SteamUser, SteamUserWithStats, DayOfWeek, Availability } from '../models/steamuser';
 import { ActivatedRoute } from '../../../node_modules/@angular/router';
 import { BellumgensApiService } from '../services/bellumgens-api.service';
-import { IgxChipsAreaComponent } from '../../../node_modules/igniteui-angular';
+import { IgxChipsAreaComponent, IgxTimePickerComponent } from '../../../node_modules/igniteui-angular';
 
 @Component({
   selector: 'app-player-details',
@@ -15,16 +15,19 @@ export class PlayerDetailsComponent {
 
   public authUser: SteamUser;
   public player: SteamUserWithStats;
-  public selectedDay: string;
+  public selectedDay: Availability;
   public weekDays = [
     'Sundays', 'Mondays', 'Tuesdays', 'Wednesdays', 'Thursdays', 'Fridays', 'Saturdays'
   ];
 
   @ViewChild(IgxChipsAreaComponent) public chips: IgxChipsAreaComponent;
+  @ViewChild('from') public from: IgxTimePickerComponent;
+  @ViewChild('to') public to: IgxTimePickerComponent;
 
   constructor(private authManager: LoginService,
               private apiService: BellumgensApiService,
-              private activatedRoute: ActivatedRoute) {
+              private activatedRoute: ActivatedRoute,
+              private cdr: ChangeDetectorRef) {
     this.authManager.steamUser.subscribe((data: SteamUser) => {
       this.authUser = data;
     });
@@ -47,6 +50,23 @@ export class PlayerDetailsComponent {
 
   public dayName(day: DayOfWeek) {
     return this.weekDays[day];
+  }
+
+  public daySelected(args: any) {
+    const index = this.chips.chipsList.toArray().indexOf(args.owner);
+    const availability = this.player.availability[index];
+    this.selectedDay = this.player.availability[index];
+    this.from.value = new Date(availability.From);
+    this.to.value = new Date(availability.To);
+    this.cdr.detectChanges();
+  }
+
+  public fromChange(args) {
+
+  }
+
+  public toChange(args) {
+
   }
 
   public playerIsUser(): boolean {
