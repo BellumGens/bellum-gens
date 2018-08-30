5697827011d67cbae91d31507a3a6802d1758dde
Konstantin Dinev <kdinev@mail.bw.edu>
2018-08-30 14:45:27 +0300

    Starting some initial implementation of the teams UI

--- src/app/services/bellumgens-api.service.ts
+++ src/app/services/bellumgens-api.service.ts
@@ -7,7 +7,7 @@ import { Observable, ReplaySubject } from 'rxjs';
   providedIn: 'root'
 })
 export class BellumgensApiService {
-  private _apiEndpoint = 'http://localhost:25702/api/users';
+  private _apiEndpoint = 'http://localhost:25702/api';
   private _rootApiEndpoint = 'http://localhost:25702';
   private _dataSubject = new ReplaySubject<SteamUserWithStats []>(1);
 
@@ -17,29 +17,33 @@ export class BellumgensApiService {
   constructor(private http: HttpClient) { }
 
   public getActiveUsers(): void {
-    this.http.get<SteamUserWithStats []>(this._apiEndpoint + '/activeusers').subscribe(
+    this.http.get<SteamUserWithStats []>(this._apiEndpoint + '/users/activeusers').subscribe(
       data => this._dataSubject.next(data),
       error => this.error = error
     );
   }
 
+  public getTeams(): void {
+
+  }
+
   public getUser(userid: string): Observable<SteamUserWithStats> {
-    return this.http.get<SteamUserWithStats>(this._apiEndpoint + '/user?userid=' + userid);
+    return this.http.get<SteamUserWithStats>(this._apiEndpoint + '/users/user?userid=' + userid);
   }
 
   public setAvailability(availability: Availability): Observable<any> {
-    return this.http.put(this._apiEndpoint + '/availability', availability, { withCredentials: true });
+    return this.http.put(this._apiEndpoint + '/users/availability', availability, { withCredentials: true });
   }
 
   public setPrimaryRole(role: Role): Observable<any> {
-    return this.http.put(this._apiEndpoint + '/primaryrole', role, { withCredentials: true });
+    return this.http.put(this._apiEndpoint + '/users/primaryrole', role, { withCredentials: true });
   }
 
   public setSecondaryRole(role: Role): Observable<any> {
-    return this.http.put(this._apiEndpoint + '/secondaryrole', role, { withCredentials: true });
+    return this.http.put(this._apiEndpoint + '/users/secondaryrole', role, { withCredentials: true });
   }
 
   public setMapPool(map: MapPool): Observable<any> {
-    return this.http.put(this._apiEndpoint + '/mapPool', map, { withCredentials: true });
+    return this.http.put(this._apiEndpoint + '/users/mapPool', map, { withCredentials: true });
   }
 }
