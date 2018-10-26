a6864bd9ddbe8af7fa45809ce26619d7181c1ab0
Konstantin Dinev <kdinev@mail.bw.edu>
2018-10-26 13:58:23 +0300

    Approving and rejecting applications.

--- src/app/team-section/team-notifications/team-notifications.component.ts
+++ src/app/team-section/team-notifications/team-notifications.component.ts
@@ -5,6 +5,8 @@ import { ApplicationUser } from 'src/app/models/applicationuser';
 import { TeamApplication } from 'src/app/models/csgoteam';
 import { BellumgensApiService } from 'src/app/services/bellumgens-api.service';
 import { LoginService } from 'src/app/services/login.service';
+import { NotificationState } from 'src/app/models/usernotifications';
+import { SuccessErrorComponent } from 'src/app/success-error/success-error.component';
 
 @Component({
   selector: 'app-team-notifications',
@@ -18,6 +20,7 @@ export class TeamNotificationsComponent implements OnInit {
   applications: TeamApplication[];
 
   @ViewChild(IgxListComponent) public notifications: IgxListComponent;
+  @ViewChild(SuccessErrorComponent) public toast: SuccessErrorComponent;
 
   constructor(private activatedRoute: ActivatedRoute,
               private apiService: BellumgensApiService,
@@ -41,4 +44,23 @@ export class TeamNotificationsComponent implements OnInit {
   ngOnInit() {
   }
 
+  public approveApplication(application: TeamApplication) {
+    this.apiService.approveApplication(application).subscribe(
+      data => {
+        application.State = NotificationState.Accepted;
+        this.toast.showSuccess('Player is now part of your team.');
+      },
+      error => this.toast.showError(error.error.Message)
+    );
+  }
+
+  public rejectApplication(application: TeamApplication) {
+    this.apiService.rejectApplication(application).subscribe(
+      data => {
+        application.State = NotificationState.Rejected;
+        this.toast.showSuccess('Player application rejected...');
+      },
+      error => this.toast.showError(error.error.Message)
+    );
+  }
 }
