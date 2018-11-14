ce24a0cce19ebf3cf1ad813b5b787aa15fa42bd9
Konstantin Dinev <kdinev@mail.bw.edu>
2018-11-14 16:20:13 +0200

    Some minor view updates

--- src/app/pipes/ordermaps.pipe.ts
+++ src/app/pipes/ordermaps.pipe.ts
@@ -1,13 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { CSGOMap, MapPool } from '../models/csgomaps';
+import { MapPool } from '../models/csgomaps';
 
 @Pipe({
   name: 'ordermaps'
 })
 export class OrdermapsPipe implements PipeTransform {
 
-  transform(mapPool: MapPool [], orderBy: string): any {
-    return mapPool.sort((x, y) => x[orderBy] < y[orderBy] ? 1 : 0);
+  transform(mapPool: MapPool []): any {
+    return mapPool.sort((x, y) => x.IsPlayed === y.IsPlayed ? 0 : x.IsPlayed < y.IsPlayed ? 1 : -1);
   }
 
 }
