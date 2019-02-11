ff2029071e0661ae8cc188faa32d987e46e2bb37
Konstantin Dinev <kdinev@mail.bw.edu>
2019-02-11 18:31:28 +0200

    Package updates + styling + icon service update

--- src/app/pipes/top-weapon-svg.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TopWeaponSvgPipe } from './top-weapon-svg.pipe';
-
-describe('TopWeaponSvgPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TopWeaponSvgPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
