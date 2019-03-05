d6a49315d863f26895564046c51df0977c5d624e
Konstantin Dinev <kdinev@mail.bw.edu>
2019-03-05 11:49:16 +0200

    chore(*): Replacing the headshot icon with svg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -206,5 +206,6 @@ export class AppModule {
     this.iconService.addSvgIcon('elite', '/assets/weapon-icons/svg_normal/weapon_elite.svg', 'weapon-icons');
     this.iconService.addSvgIcon('sawedoff', '/assets/weapon-icons/svg_normal/weapon_sawedoff.svg', 'weapon-icons');
     this.iconService.addSvgIcon('taser', '/assets/weapon-icons/svg_normal/weapon_taser.svg', 'weapon-icons');
+    this.iconService.addSvgIcon('headshot', '/assets/headshot24x24.svg', 'weapon-icons');
   }
 }
