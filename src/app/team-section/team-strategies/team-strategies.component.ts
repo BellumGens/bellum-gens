d5422cb14a099ff37e59b7637ecde4d39b9fec93
Konstantin Dinev <kdinev@mail.bw.edu>
2019-03-21 16:01:03 +0200

    feat(strats): Twitch videos can be added as strats

--- src/app/team-section/team-strategies/team-strategies.component.ts
+++ src/app/team-section/team-strategies/team-strategies.component.ts
@@ -30,6 +30,7 @@ export class TeamStrategiesComponent implements OnInit {
   isEditor = false;
 
   private _youtubeRegEx = /(youtu\.be\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v)\/))([^\?&"'>]+)/;
+  private _twitchRegEx = /(twitch\.tv\/)(videos\/|\?[^\?"'>]+video\=v)([^\?&"'>]+)/;
 
   @ViewChild(IgxDialogComponent) public dialog: IgxDialogComponent;
 
@@ -107,18 +108,34 @@ export class TeamStrategiesComponent implements OnInit {
     );
   }
 
+  public isVideo(url: string) {
+    return this.isYoutube(url) || this.isTwitch(url);
+  }
+
   public isYoutube(url: string) {
     return this._youtubeRegEx.test(url);
   }
 
+  public isTwitch(url: string) {
+    return this._twitchRegEx.test(url);
+  }
+
   public getVideoEmbedLink() {
-    if (this._youtubeRegEx.test(this.newStrategy.Url)) {
+    if (this.isYoutube(this.newStrategy.Url)) {
       const parts = this._youtubeRegEx.exec(this.newStrategy.Url);
       if (this.videoId && this.videoId === parts[5]) {
         return true;
       }
       this.videoId = parts[5];
       this.newStrategy.Url = this.getYoutubeEmbedLink(this.newStrategy.Url);
+      return true;
+    } else if (this.isTwitch(this.newStrategy.Url)) {
+      const parts = this._twitchRegEx.exec(this.newStrategy.Url);
+      if (this.videoId && this.videoId === parts[3]) {
+        return true;
+      }
+      this.videoId = parts[3];
+      this.newStrategy.Url = this.getTwitchEmbedLink(this.newStrategy.Url);
       return true;
     }
     return false;
@@ -127,6 +144,11 @@ export class TeamStrategiesComponent implements OnInit {
   public getYoutubeEmbedLink(url: string): string {
     const parts = this._youtubeRegEx.exec(url);
     return `https://www.youtube.com/embed/${parts[5]}`;
+  }
+
+  public getTwitchEmbedLink(url: string): string {
+    const parts = this._twitchRegEx.exec(url);
+    return `https://player.twitch.tv/?autoplay=false&video=v${parts[3]}`;
   }
 
   public deleteStrat(args: TeamStrategy) {
