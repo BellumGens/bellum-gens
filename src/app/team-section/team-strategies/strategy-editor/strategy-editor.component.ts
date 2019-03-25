c81d95ba6579d4abc7b440324f338589dee262fb
Konstantin Dinev <kdinev@mail.bw.edu>
2019-03-25 18:08:08 +0200

    feat(editor): Initial commit for this feature #36

---
+++ src/app/team-section/team-strategies/strategy-editor/strategy-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-strategy-editor',
+  templateUrl: './strategy-editor.component.html',
+  styleUrls: ['./strategy-editor.component.css']
+})
+export class StrategyEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
