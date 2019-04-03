c05b688a3550d50d338aa6ecac949c9fb400c2ed
Konstantin Dinev <kdinev@mail.bw.edu>
2019-04-03 16:47:58 +0300

    feat(strat editor): Adding initial commit

--- src/app/team-section/team-strategies/strategy-editor/strategy-editor.component.ts
+++ src/app/team-section/team-strategies/strategy-editor/strategy-editor.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ActiveDutyDescriptor, ActiveDuty } from 'src/app/models/csgomaps';
 
 @Component({
   selector: 'app-strategy-editor',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./strategy-editor.component.css']
 })
 export class StrategyEditorComponent implements OnInit {
+  public maps: ActiveDutyDescriptor [] = ActiveDuty;
+
+  private _activeMap: ActiveDutyDescriptor;
+
+  public get map() {
+    return this._activeMap;
+  }
+
+  public set map(map: ActiveDutyDescriptor) {
+    this._activeMap = map;
+    this.renderContext();
+  }
+
+  private context;
+
+  @ViewChild('board') public canvas: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  public changeMap(map: ActiveDutyDescriptor) {
+    this.map = map;
+  }
+
+  public renderContext() {
+    this.context = this.canvas.nativeElement.getContext('2d');
+    const image = new Image();
+    image.src = this._activeMap.radar[0];
+    image.onload = () => {
+      this.context.drawImage(image, 0, 0, 1024, 1024);
+    };
+  }
 }
