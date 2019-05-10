e1f35808023983f3b6e41b79ef70465042422151
Konstantin Dinev <kdinev@mail.bw.edu>
2019-05-10 16:08:50 +0300

    fix(tests): Fixing tests

--- src/app/add-steam/add-steam.component.spec.ts
+++ src/app/add-steam/add-steam.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddSteamComponent } from './add-steam.component';
+import { IgxIconModule } from 'igniteui-angular';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AddSteamComponent', () => {
   let component: AddSteamComponent;
@@ -8,6 +11,11 @@ describe('AddSteamComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        ServiceWorkerModule.register('', {enabled: false}),
+        IgxIconModule
+      ],
       declarations: [ AddSteamComponent ]
     })
     .compileComponents();
