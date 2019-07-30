20791976250ec9ef7fb03cac7735c6e259cbb2fc
Konstantin Dinev <kdinev@mail.bw.edu>
2019-07-30 10:40:34 +0300

    chore(*): Updating packages and fixing tests

--- src/app/team-section/team-strategies/strategy-details/strategy-details.component.spec.ts
+++ src/app/team-section/team-strategies/strategy-details/strategy-details.component.spec.ts
@@ -4,7 +4,11 @@ import { StrategyDetailsComponent } from './strategy-details.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { IgxCardModule } from 'igniteui-angular';
+import { IgxCardModule, IgxIconModule, IgxListModule, IgxInputGroupModule, IgxDividerModule, IgxAvatarModule } from 'igniteui-angular';
+import { VotesPipe } from 'src/app/pipes/votes.pipe';
+import { IsVideoPipe } from 'src/app/pipes/is-video.pipe';
+import { SafeVideoLinkPipe } from 'src/app/pipes/safe-video-link.pipe';
+import { FormsModule } from '@angular/forms';
 
 describe('StrategyDetailsComponent', () => {
   let component: StrategyDetailsComponent;
@@ -15,10 +19,21 @@ describe('StrategyDetailsComponent', () => {
       imports: [
         RouterTestingModule,
         HttpClientTestingModule,
+        FormsModule,
         ServiceWorkerModule.register('', {enabled: false}),
-        IgxCardModule
+        IgxCardModule,
+        IgxIconModule,
+        IgxListModule,
+        IgxInputGroupModule,
+        IgxDividerModule,
+        IgxAvatarModule
       ],
-      declarations: [ StrategyDetailsComponent ]
+      declarations: [
+        StrategyDetailsComponent,
+        VotesPipe,
+        IsVideoPipe,
+        SafeVideoLinkPipe
+      ]
     })
     .compileComponents();
   }));
