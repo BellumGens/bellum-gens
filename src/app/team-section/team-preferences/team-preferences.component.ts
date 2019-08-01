239730beef8373f6c6966f297c901d78423e37ac
Konstantin Dinev <kdinev@mail.bw.edu>
2019-08-01 14:19:51 +0300

    chore(*): Fixing more imports

--- src/app/team-section/team-preferences/team-preferences.component.ts
+++ src/app/team-section/team-preferences/team-preferences.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { BellumgensApiService } from '../../services/bellumgens-api.service';
 import { CSGOTeam, TeamMember } from '../../models/csgoteam';
 import { SteamUserSummary } from '../../models/steamuser';
-import { BaseComponent } from 'src/app/base/base.component';
+import { BaseComponent } from '../../base/base.component';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
