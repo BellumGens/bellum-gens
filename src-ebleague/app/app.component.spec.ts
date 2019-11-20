0458d5cd7a1ed82424624f8e30fb24537c054a26
Konstantin Dinev <kdinev@mail.bw.edu>
2019-11-20 11:54:52 +0200

    tests(*): Fixing imports

--- src-ebleague/app/app.component.spec.ts
+++ src-ebleague/app/app.component.spec.ts
@@ -25,31 +25,31 @@ import { IgxNavigationDrawerModule,
   IgxSwitchModule,
   IgxDividerModule,
   IgxSelectModule} from 'igniteui-angular';
-import { SuccessErrorComponent } from './success-error/success-error.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { TeamNavComponent } from './team-section/team-nav/team-nav.component';
-import { LoginComponent } from './login/login.component';
-import { QuickSearchComponent } from './search/quick-search/quick-search.component';
-import { SearchComponent } from './search/search/search.component';
-import { TeamNotificationsComponent } from './team-section/team-notifications/team-notifications.component';
-import { PlayerNotificationsComponent } from './player-section/notifications/notifications.component';
+import { SuccessErrorComponent } from '../../src-bellumgens/app/success-error/success-error.component';
+import { NotificationsComponent } from '../../src-bellumgens/app/notifications/notifications.component';
+import { TeamNavComponent } from '../../src-bellumgens/app/team-section/team-nav/team-nav.component';
+import { LoginComponent } from '../../src-bellumgens/app/login/login.component';
+import { QuickSearchComponent } from '../../src-bellumgens/app/search/quick-search/quick-search.component';
+import { SearchComponent } from '../../src-bellumgens/app/search/search/search.component';
+import { TeamNotificationsComponent } from '../../src-bellumgens/app/team-section/team-notifications/team-notifications.component';
+import { PlayerNotificationsComponent } from '../../src-bellumgens/app/player-section/notifications/notifications.component';
 import { FormsModule } from '@angular/forms';
-import { ConfirmComponent } from './confirm/confirm.component';
-import { GroupsFilterPipe } from './pipes/groups-filter.pipe';
-import { ReduceQuickSearchResultPipe } from './pipes/reduce-quick-search-result.pipe';
-import { PlayerSearchComponent } from './search/player-search/player-search.component';
-import { TeamSearchComponent } from './search/team-search/team-search.component';
-import { DisabledNotificationsPipe } from './pipes/disabled-notifications.pipe';
-import { SortNotificationsPipe } from './pipes/sort-notifications.pipe';
-import { SortApplicationsPipe } from './pipes/sort-applications.pipe';
+import { ConfirmComponent } from '../../src-bellumgens/app/confirm/confirm.component';
+import { GroupsFilterPipe } from '../../src-bellumgens/app/pipes/groups-filter.pipe';
+import { ReduceQuickSearchResultPipe } from '../../src-bellumgens/app/pipes/reduce-quick-search-result.pipe';
+import { PlayerSearchComponent } from '../../src-bellumgens/app/search/player-search/player-search.component';
+import { TeamSearchComponent } from '../../src-bellumgens/app/search/team-search/team-search.component';
+import { DisabledNotificationsPipe } from '../../src-bellumgens/app/pipes/disabled-notifications.pipe';
+import { SortNotificationsPipe } from '../../src-bellumgens/app/pipes/sort-notifications.pipe';
+import { SortApplicationsPipe } from '../../src-bellumgens/app/pipes/sort-applications.pipe';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { UserPreferencesComponent } from './player-section/user-preferences/user-preferences.component';
-import { NotificationStatePipe } from './pipes/notification-state.pipe';
-import { PlayerCountryPipe } from './pipes/player-country.pipe';
-import { LoginDialogComponent } from './login/login-dialog/login-dialog.component';
-import { TeamNewComponent } from './team-section/team-new/team-new.component';
+import { UserPreferencesComponent } from '../../src-bellumgens/app/player-section/user-preferences/user-preferences.component';
+import { NotificationStatePipe } from '../../src-bellumgens/app/pipes/notification-state.pipe';
+import { PlayerCountryPipe } from '../../src-bellumgens/app/pipes/player-country.pipe';
+import { LoginDialogComponent } from '../../src-bellumgens/app/login/login-dialog/login-dialog.component';
+import { TeamNewComponent } from '../../src-bellumgens/app/team-section/team-new/team-new.component';
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -97,7 +97,6 @@ describe('AppComponent', () => {
         LoginComponent,
         LoginDialogComponent,
         QuickSearchComponent,
-        SearchComponent,
         TeamNotificationsComponent,
         PlayerNotificationsComponent,
         ConfirmComponent,
