821f3c8d4af400884619af7a67cc84afc6d990e6
Konstantin Dinev <kdinev@mail.bw.edu>
2019-11-20 14:49:58 +0200

    feat(*): Adjusting the server config

--- webpack.bellumgens.server.config.js
+++ webpack.bellumgens.server.config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   output: {
     // Puts the output at the root of the dist folder
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'dist-bellumgens'),
     filename: '[name].js'
   },
   module: {
