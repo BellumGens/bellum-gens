450f92a7f6a616b738eb940aaba0833896d18bd9
Konstantin Dinev <kdinev@mail.bw.edu>
2019-11-22 17:22:49 +0200

    chore(*)...

--- src-bellumgens/app/app.server.module.ts
+++ src-bellumgens/app/app.server.module.ts
@@ -4,13 +4,11 @@ import { ServerModule } from '@angular/platform-server';
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
-import { AppShellComponent } from './app-shell/app-shell.component';
 import { IgxProgressBarModule } from 'igniteui-angular';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/app-shell', pathMatch: 'full' },
-  { path: 'app-shell', component: AppShellComponent }
+  { path: '', redirectTo: '/app-shell', pathMatch: 'full' }
 ];
 
 @NgModule({
