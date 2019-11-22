39facbe8ae03dea881ccdc039b7805a2e59c88a1
Konstantin Dinev <kdinev@mail.bw.edu>
2019-11-22 17:53:58 +0200

    chore(*): I thought I removed this

--- src-bellumgens/app/app-routing.module.ts
+++ src-bellumgens/app/app-routing.module.ts
@@ -48,8 +48,6 @@ export const routes: Routes = [
   { path: 'team/:teamid/:stratid', component: StrategyEditorComponent },
   { path: 'search/teams/:query', component: TeamResultsComponent },
   { path: 'search/players/:query', component: PlayerResultsComponent },
-  { path: 'tournament-signup', redirectTo: 'https://eb-league.com' },
-  { path: 'tournament/format', redirectTo: 'https://eb-league.com' },
   { path: '**', component: HomeComponent }
 ];
 
