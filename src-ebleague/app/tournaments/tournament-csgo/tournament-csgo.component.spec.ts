5000f7b55e9982d9147ab5f71dd41364521486d7
Konstantin Dinev <kdinev@mail.bw.edu>
2020-01-06 14:25:47 +0200

    feat(*): adding empty group list #114

--- src-ebleague/app/tournaments/tournament-csgo/tournament-csgo.component.spec.ts
+++ src-ebleague/app/tournaments/tournament-csgo/tournament-csgo.component.spec.ts
@@ -4,7 +4,7 @@ import { TournamentCsgoComponent } from './tournament-csgo.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { IgxAvatarModule, IgxCardModule, IgxBadgeModule } from 'igniteui-angular';
+import { IgxAvatarModule, IgxCardModule, IgxBadgeModule, IgxListModule } from 'igniteui-angular';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('TournamentCsgoComponent', () => {
@@ -21,7 +21,8 @@ describe('TournamentCsgoComponent', () => {
         ServiceWorkerModule.register('', {enabled: false}),
         IgxAvatarModule,
         IgxCardModule,
-        IgxBadgeModule
+        IgxBadgeModule,
+        IgxListModule
       ]
     })
     .compileComponents();
--- src-ebleague/app/tournaments/tournament-sc2/tournament-sc2.component.spec.ts
+++ src-ebleague/app/tournaments/tournament-sc2/tournament-sc2.component.spec.ts
@@ -4,7 +4,7 @@ import { TournamentSc2Component } from './tournament-sc2.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { IgxAvatarModule, IgxCardModule, IgxBadgeModule } from 'igniteui-angular';
+import { IgxAvatarModule, IgxCardModule, IgxBadgeModule, IgxListModule } from 'igniteui-angular';
 import { RouterTestingModule } from '@angular/router/testing';
 
 describe('TournamentSc2Component', () => {
@@ -21,7 +21,8 @@ describe('TournamentSc2Component', () => {
         ServiceWorkerModule.register('', {enabled: false}),
         IgxAvatarModule,
         IgxCardModule,
-        IgxBadgeModule
+        IgxBadgeModule,
+        IgxListModule
       ]
     })
     .compileComponents();
