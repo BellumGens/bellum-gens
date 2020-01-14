b1097cbdb7907e170183e85bcfa2cb6976bda2b9
Konstantin Dinev <kdinev@mail.bw.edu>
2020-01-14 20:40:28 +0200

    feat(*): adding more admin views

--- src-ebleague/app/admin/admin.component.ts
+++ src-ebleague/app/admin/admin.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { LoginService } from '../../../src-common/services/login.service';
 import { ApplicationUser } from '../../../src-common/models/applicationuser';
+import { ApiTournamentsService } from '../../../src-common/services/bellumgens-api.tournaments.service';
+import { Tournament } from '../../../src-common/models/tournament';
 
 @Component({
   selector: 'app-admin',
@@ -10,10 +12,13 @@ import { ApplicationUser } from '../../../src-common/models/applicationuser';
 export class AdminComponent {
   public roles: string [];
   public users: ApplicationUser [];
+  public tournaments: Tournament [];
 
-  constructor(private authService: LoginService) {
+  constructor(private authService: LoginService,
+              private apiService: ApiTournamentsService) {
     this.authService.getUserRoles().subscribe(data => this.roles = data);
     this.authService.getUsers().subscribe(data => this.users = data);
+    this.apiService.tournaments.subscribe(data => this.tournaments = data);
   }
 
   public submitRole(role: string) {
