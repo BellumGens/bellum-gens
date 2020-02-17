07d7c8fc2dcb84044e6ef7c1b2dfa7b7a23c6aeb
Konstantin Dinev <kdinev@mail.bw.edu>
2020-02-17 19:04:07 +0200

    refactor(*): a lot of things

--- src-bellumgens/app/home/home.component.ts
+++ src-bellumgens/app/home/home.component.ts
@@ -1,14 +1,14 @@
-import { Component, HostListener, PLATFORM_ID, ViewChild, Inject } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 import { LoginService } from '../../../src-common/services/login.service';
 import { ApplicationUser } from '../../../src-common/models/applicationuser';
 import { SocialMediaService } from '../../../src-common/services/social-media.service';
 import { CommunicationService } from '../../../src-common/services/communication.service';
 import { environment } from '../../../src-common/environments/environment';
-import { isPlatformBrowser } from '@angular/common';
 import { BaseComponent } from '../base/base.component';
 import { Title, Meta } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { IgxCarouselComponent } from 'igniteui-angular';
+import { ApiTournamentsService } from '../../../src-common/services/bellumgens-api.tournaments.service';
 
 @Component({
   selector: 'app-home',
@@ -19,23 +19,27 @@ export class HomeComponent extends BaseComponent {
   public authUser: ApplicationUser;
   public navigation = true;
   public environment = environment;
+  public userEmail: string = null;
 
   @ViewChild(IgxCarouselComponent, { static: true })
   public carousel: IgxCarouselComponent;
 
-  private isBrowser: boolean;
-
-  constructor(@Inject(PLATFORM_ID) platformId: Object,
-              private authManager: LoginService,
+  constructor(private authManager: LoginService,
+              private apiService: ApiTournamentsService,
               private socialMedia: SocialMediaService,
               private commService: CommunicationService,
               titleService: Title,
               meta: Meta,
               activeRoute: ActivatedRoute) {
     super(titleService, meta, activeRoute);
-    this.isBrowser = isPlatformBrowser(platformId);
-    this.navigation = this.isBrowser ? window.matchMedia('(min-width: 768px)').matches : true;
+    this.navigation = window.matchMedia('(min-width: 768px)').matches;
     this.authManager.applicationUser.subscribe(data => this.authUser = data);
+  }
+
+  public subscribe() {
+    if (this.userEmail) {
+      this.apiService.addSubscriber(this.userEmail).subscribe();
+    }
   }
 
   public tweet() {
