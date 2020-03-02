472c3052c67a8530a2dc269f86acada1f2d2e2e3
Konstantin Dinev <kdinev@mail.bw.edu>
2020-03-02 23:18:06 +0200

    chore(*): angular ssr is broken again

--- src-common/guards/eventadmin.guard.ts
+++ src-common/guards/eventadmin.guard.ts
@@ -1,18 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoginService } from '../services/login.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventAdminGuard implements CanActivate {
-  constructor(private authService: LoginService) {}
+  private isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) platformId: string, private authService: LoginService) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.isEventAdmin();
+    return this.isBrowser ? this.isEventAdmin() : true;
   }
 
   isEventAdmin() {
--- src-common/guards/teamadmin.guard.ts
+++ src-common/guards/teamadmin.guard.ts
@@ -1,18 +1,23 @@
-import { Injectable } from '@angular/core';
+import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoginService } from '../services/login.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TeamadminGuard implements CanActivate {
-  constructor(private authService: LoginService) { }
+  private isBrowser: boolean;
+
+  constructor(@Inject(PLATFORM_ID) platformId: string, private authService: LoginService) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.isTeamAdmin(next.parent.params.teamid);
+    return this.isBrowser ? this.isTeamAdmin(next.parent.params.teamid) : true;
   }
 
   isTeamAdmin(teamid: string) {
