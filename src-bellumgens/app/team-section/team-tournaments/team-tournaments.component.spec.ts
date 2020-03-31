d9eb5602018e10cce18e9475de09f6e911055a60
Konstantin Dinev <kdinev@mail.bw.edu>
2020-03-31 13:03:13 +0300

    feat(*): team view tabs updated #160

--- src-bellumgens/app/team-section/team-tournaments/team-tournaments.component.spec.ts
+++ src-bellumgens/app/team-section/team-tournaments/team-tournaments.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TeamTournamentsComponent } from './team-tournaments.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { IgxGridModule, IgxAvatarModule } from 'igniteui-angular';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('TeamTournamentsComponent', () => {
   let component: TeamTournamentsComponent;
@@ -11,7 +13,10 @@ describe('TeamTournamentsComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ TeamTournamentsComponent ],
       imports: [
-        HttpClientTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule,
+        IgxGridModule,
+        IgxAvatarModule
       ]
     })
     .compileComponents();
--- src-bellumgens/app/team-section/team-tournaments/team-tournaments.component.ts
+++ src-bellumgens/app/team-section/team-tournaments/team-tournaments.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Tournament } from '../../../../src-common/models/tournament';
 import { ActivatedRoute } from '@angular/router';
 import { BellumgensApiService } from '../../../../src-common/services/bellumgens-api.service';
+import { CSGOTeam } from 'src-common/models/csgoteam';
 
 @Component({
   selector: 'app-team-tournaments',
@@ -10,12 +11,15 @@ import { BellumgensApiService } from '../../../../src-common/services/bellumgens
 })
 export class TeamTournamentsComponent {
   public tournaments: Tournament [];
+  public team: CSGOTeam;
+  public emptyGuid = '00000000-0000-0000-0000-000000000000';
 
   constructor(private apiService: BellumgensApiService,
               private activatedRoute: ActivatedRoute) {
     this.activatedRoute.parent.params.subscribe(params => {
       const teamId = params['teamid'];
       if (teamId) {
+        this.apiService.getTeam(teamId).subscribe(team => this.team = team);
         this.apiService.getTeamTournaments(teamId).subscribe(data => this.tournaments = data);
       }
     });
