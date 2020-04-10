53f51cc2770730ce51c4838edd2af039a57fb04d
Konstantin Dinev <kdinev@mail.bw.edu>
2020-04-11 00:25:58 +0300

    fix(*): build

--- src-ebleague/app/shop/shop.component.spec.ts
+++ src-ebleague/app/shop/shop.component.spec.ts
@@ -3,9 +3,10 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ShopComponent } from './shop.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { IgxSelectModule, IgxInputGroupModule, IgxIconModule, IgxButtonModule, IgxMaskModule } from 'igniteui-angular';
+import { IgxSelectModule, IgxInputGroupModule, IgxIconModule, IgxButtonModule, IgxMaskModule, IgxDividerModule } from 'igniteui-angular';
 import { FilterSizesPipe } from '../pipes/filter-sizes.pipe';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('ShopComponent', () => {
   let component: ShopComponent;
@@ -18,11 +19,13 @@ describe('ShopComponent', () => {
         FormsModule,
         HttpClientTestingModule,
         RouterTestingModule,
+        NoopAnimationsModule,
         IgxSelectModule,
         IgxInputGroupModule,
         IgxIconModule,
         IgxButtonModule,
-        IgxMaskModule
+        IgxMaskModule,
+        IgxDividerModule
       ]
     })
     .compileComponents();
