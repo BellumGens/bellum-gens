3f4b04e0404ccb56eaff1e701f5322a72593a1ab
Konstantin Dinev <kdinev@mail.bw.edu>
2020-04-10 23:35:47 +0300

    fix(*): tests

--- src-common/services/bellumgens-api.shop.service.spec.ts
+++ src-common/services/bellumgens-api.shop.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ApiShopService } from './bellumgens-api.shop.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('BellumgensApi.ShopService', () => {
   let service: ApiShopService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
     service = TestBed.inject(ApiShopService);
   });
 
