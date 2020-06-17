5a288c5aa8934866af2ffb21c8fecaa95559d16e
Konstantin Dinev <kdinev@mail.bw.edu>
2020-06-17 09:57:29 +0300

    fix(*): lint errors

--- src-ebleague/app/shop/shop.component.spec.ts
+++ src-ebleague/app/shop/shop.component.spec.ts
@@ -3,7 +3,15 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ShopComponent } from './shop.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { IgxSelectModule, IgxInputGroupModule, IgxIconModule, IgxButtonModule, IgxMaskModule, IgxDividerModule, IgxTextSelectionModule } from '@infragistics/igniteui-angular';
+import {
+  IgxSelectModule,
+  IgxInputGroupModule,
+  IgxIconModule,
+  IgxButtonModule,
+  IgxMaskModule,
+  IgxDividerModule,
+  IgxTextSelectionModule
+} from '@infragistics/igniteui-angular';
 import { FilterSizesPipe } from '../pipes/filter-sizes.pipe';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
