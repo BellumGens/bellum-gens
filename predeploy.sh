1537beba4d0c5ea169442e1d6b7f9157a6ceca81
Konstantin Dinev <kdinev@mail.bw.edu>
2020-06-26 11:53:38 +0300

    chore(ssr): fixing it again

--- predeploy.sh
+++ predeploy.sh
@@ -1,2 +1,5 @@
-mv dist/bellumgens/server/main.js dist/bellumgens/server.js
-mv dist/ebleague/server/main.js dist/ebleague/server.js
+mv dist/bellumgens/server/main.js dist/bellumgens/main.js
+mv dist/ebleague/server/main.js dist/ebleague/main.js
+mv dist/ebleague/server/admin-admin-module.js dist/ebleague/admin-admin-module.js
+mv dist/ebleague/server/shop-shop-module.js dist/ebleague/shop-shop-module.js
+mv dist/ebleague/server/tournaments-tournament-module.js dist/ebleague/tournaments-tournament-module.js
