f74c8a51bf26ca482e125c0c71a1180b79375236
Konstantin Dinev <kdinev@mail.bw.edu>
2020-11-14 12:12:54 +0200

    feat(*): lets see if cookies are fine now

--- src-bellumgens/app/app.server.module.ts
+++ src-bellumgens/app/app.server.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
 import { ServerTransferStateModule } from '@angular/platform-server';
+import { XhrFactory } from '@angular/common/http';
+
+import * as xhr2 from 'xhr2';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+
+export class ServerXhr implements XhrFactory {
+  build(): XMLHttpRequest {
+    xhr2.prototype._restrictedHeaders.cookie = false;
+    return new xhr2.XMLHttpRequest();
+  }
+}
 
 
 @NgModule({
@@ -13,6 +23,7 @@ import { AppComponent } from './app.component';
     ServerTransferStateModule
   ],
   bootstrap: [AppComponent],
+  providers: [{ provide: XhrFactory, useClass: ServerXhr }],
   declarations: [],
 })
 export class AppServerModule {
