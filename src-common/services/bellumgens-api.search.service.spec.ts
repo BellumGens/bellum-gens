98716b0a31e619b7593f68b5be1ca691f80c7b83
Konstantin Dinev <kdinev@mail.bw.edu>
2020-12-07 19:24:46 +0200

    fix(*): tests

--- src-common/services/bellumgens-api.search.service.spec.ts
+++ src-common/services/bellumgens-api.search.service.spec.ts
@@ -1,12 +1,15 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ApiSearchService } from './bellumgens-api.search.service';
 
-describe('BellumgensApi.SearchService', () => {
+describe('ApiSearchService', () => {
   let service: ApiSearchService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
     service = TestBed.inject(ApiSearchService);
   });
 
