c5fac0e44da611c43a574f9fcdf69cd7b85d842c
Konstantin Dinev <kdinev@mail.bw.edu>
2020-12-16 17:33:47 +0200

    fix(*): tests

--- src-common/services/bellumgens-api.strategies.service.spec.ts
+++ src-common/services/bellumgens-api.strategies.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ApiStrategiesService } from './bellumgens-api.strategies.service';
@@ -6,7 +7,7 @@ describe('ApiStrategiesService', () => {
   let service: ApiStrategiesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({imports: [ HttpClientTestingModule ]});
     service = TestBed.inject(ApiStrategiesService);
   });
 
