137c3edeebca285c6ca7f527470e2b25bdd82059
Konstantin Dinev <kdinev@mail.bw.edu>
2021-09-10 16:43:57 +0300

    chore(*): cleaning up player component

--- src-bellumgens/app/player-section/player.component.ts
+++ src-bellumgens/app/player-section/player.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Title, Meta } from '@angular/platform-browser';
 
@@ -21,8 +21,7 @@ import { SortWeaponsPipe } from '../pipes/sort-weapons.pipe';
 
 @Component({
   templateUrl: './player.component.html',
-  styleUrls: ['./player.component.scss'],
-  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./player.component.scss']
 })
 export class PlayerComponent extends BaseComponent {
   public authUser: ApplicationUser;
