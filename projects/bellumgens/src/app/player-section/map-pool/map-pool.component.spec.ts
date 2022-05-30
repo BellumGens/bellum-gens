9e99cc451d1be992b13fac90f9b7c20cabb5d932
Konstantin Dinev <kdinev@mail.bw.edu>
2022-05-30 17:36:44 +0300

    chore(*): fixing tests

--- projects/bellumgens/src/app/player-section/map-pool/map-pool.component.spec.ts
+++ projects/bellumgens/src/app/player-section/map-pool/map-pool.component.spec.ts
@@ -3,6 +3,8 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MapPoolComponent } from './map-pool.component';
 import { IgxCheckboxModule, IgxCardModule } from '@infragistics/igniteui-angular';
 import { BellumGensModule } from 'projects/common/src/public_api';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 describe('MapPoolComponent', () => {
   let component: MapPoolComponent;
@@ -11,8 +13,10 @@ describe('MapPoolComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       imports: [
+        FormsModule,
         IgxCheckboxModule,
         IgxCardModule,
+        NoopAnimationsModule,
         BellumGensModule
       ],
       declarations: [
