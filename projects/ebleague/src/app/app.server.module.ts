6a3ced04d774a5205adfcb089c82c32e06961257
Konstantin Dinev <kdinev@mail.bw.edu>
2023-07-19 18:17:16 +0300

    feat(ssr): updating server code

--- projects/ebleague/src/app/app.server.module.ts
+++ projects/ebleague/src/app/app.server.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-import { ServerTransferStateModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
+import { provideClientHydration } from '@angular/platform-browser';
 
 @NgModule({
   imports: [
     AppModule,
-    ServerModule,
-    ServerTransferStateModule
+    ServerModule
   ],
+  providers: [provideClientHydration()],
   bootstrap: [AppComponent],
   declarations: [],
 })
