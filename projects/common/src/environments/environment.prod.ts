f223f87dc0f9e5fcc78164f6af50f9ec7aca4271
Konstantin Dinev <kdinev@mail.bw.edu>
2023-07-20 14:29:55 +0300

    chore(*): trying different path to browser/

--- projects/common/src/environments/environment.prod.ts
+++ projects/common/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
   rootApiEndpoint: 'https://api.bellumgens.com',
   authApiEndpoint: 'https://api.bellumgens.com/api/account',
   apiEndpoint: 'https://api.bellumgens.com/api',
-  distFolderEbleague: 'browser',
-  distFolderBellumGens: 'browser',
+  distFolderEbleague: '../browser',
+  distFolderBellumGens: '../browser',
   VAPID_PUBLIC_KEY: 'BNmXyw15axbl9tOAKz0ybCcfui-w4WiILSO_PRvU2qF0gdB56C0jRieaFSncZnpaulsyl0c3VUsRCyOR4foil50'
 };
