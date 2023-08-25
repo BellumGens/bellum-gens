7b70d26ee7db0e0b37bed257eac3a626c597493e
Konstantin Dinev <kdinev@mail.bw.edu>
2023-08-25 14:23:37 +0300

    refactor(eb-league): lazy-loading components

--- projects/ebleague/src/app/app-routing.module.ts
+++ projects/ebleague/src/app/app-routing.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { RaffleComponent } from './raffle/raffle.component';
-import { RegistrationSuccessComponent } from './tournament-registration/registration-success/registration-success.component';
-import { RegistrationComponent, UnauthorizedComponent } from '../../../common/src/public_api';
-import { NewsComponent } from './news/news.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'giveaway', redirectTo: 'raffle', pathMatch: 'full' },
-  { path: 'raffle', component: RaffleComponent },
-  { path: 'register', component: RegistrationComponent },
-  { path: 'news', component: NewsComponent, data: {
+  { path: 'raffle', loadComponent: () => import('./raffle/raffle.component').then(m => m.RaffleComponent) },
+  { path: 'register', loadComponent: () => import('../../../common/src/public_api').then(m => m.RegistrationComponent) },
+  { path: 'news', loadComponent: () => import('./news/news.component').then(m => m.NewsComponent), data: {
       title: 'Esports Business League - News',
       twitterTitle: 'Esports Business League - News',
       description: 'Esports Business League is an amateur esports competition at the workplace in CS:GO and StarCraft II',
@@ -19,9 +15,9 @@ export const routes: Routes = [
       image: '/assets/crew/sugarbunny.jpg'
     }
   },
-  { path: 'registration-success', component: RegistrationSuccessComponent },
+  { path: 'registration-success', loadComponent: () => import('./tournament-registration/registration-success/registration-success.component').then(m => m.RegistrationSuccessComponent) },
   { path: 'unauthorized', redirectTo: 'unauthorized/', pathMatch: 'full' },
-  { path: 'unauthorized/:message', component: UnauthorizedComponent },
+  { path: 'unauthorized/:message', loadComponent: () => import('../../../common/src/public_api').then(m => m.UnauthorizedComponent) },
   { path: 'format', redirectTo: '/tournament/format', pathMatch: 'full' },
   { path: 'csgo', redirectTo: '/tournament/csgo/', pathMatch: 'full' },
   { path: 'sc2', redirectTo: '/tournament/sc2/', pathMatch: 'full' },
