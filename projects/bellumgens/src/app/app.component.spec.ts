80c15e650101fabd703ab2102edebeae30f6dad3
Konstantin Dinev <kdinev@mail.bw.edu>
2023-11-15 17:46:21 +0200

    feat(standalone): converting bellum-gens

--- projects/bellumgens/src/app/app.component.spec.ts
+++ projects/bellumgens/src/app/app.component.spec.ts
@@ -1,48 +1,10 @@
 import { TestBed, ComponentFixture, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { IgxNavigationDrawerModule,
-  IgxNavbarModule,
-  IgxLayoutModule,
-  IgxRippleModule,
-  IgxIconModule,
-  IgxDropDownModule,
-  IgxInputGroupModule,
-  IgxToggleModule,
-  IgxBadgeModule,
-  IgxBannerModule,
-  IgxButtonModule,
-  IgxSnackbarModule,
-  IgxAvatarModule,
-  IgxCardModule,
-  IgxListModule,
-  IgxDialogModule,
-  IgxProgressBarModule,
-  IgxButtonGroupModule,
-  IgxSliderModule,
-  IgxRadioModule,
-  IgxTabsModule,
-  IgxSwitchModule,
-  IgxDividerModule,
-  IgxSelectModule} from '@infragistics/igniteui-angular';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { TeamNavComponent } from './team-section/team-nav/team-nav.component';
-import { QuickSearchComponent } from './search/quick-search/quick-search.component';
-import { SearchComponent } from './search/search/search.component';
 import { FormsModule } from '@angular/forms';
-import { GroupsFilterPipe } from './pipes/groups-filter.pipe';
-import { ReduceQuickSearchResultPipe } from './pipes/reduce-quick-search-result.pipe';
-import { PlayerSearchComponent } from './search/player-search/player-search.component';
-import { TeamSearchComponent } from './search/team-search/team-search.component';
-import { DisabledNotificationsPipe } from './pipes/disabled-notifications.pipe';
-import { SortNotificationsPipe } from './pipes/sort-notifications.pipe';
-import { SortApplicationsPipe } from './pipes/sort-applications.pipe';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { NotificationStatePipe } from './pipes/notification-state.pipe';
-import { TeamNewComponent } from './team-section/team-new/team-new.component';
-import { LanguagesComponent, LoginComponent, SuccessErrorComponent } from 'projects/common/src/public_api';
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -56,49 +18,8 @@ describe('AppComponent', () => {
         FormsModule,
         NoopAnimationsModule,
         ServiceWorkerModule.register('', { enabled: false }),
-        IgxNavigationDrawerModule,
-        IgxNavbarModule,
-        IgxLayoutModule,
-        IgxIconModule,
-        IgxToggleModule,
-        IgxDropDownModule,
-        IgxInputGroupModule,
-        IgxBadgeModule,
-        IgxBannerModule,
-        IgxButtonModule,
-        IgxNavbarModule,
-        IgxSnackbarModule,
-        IgxAvatarModule,
-        IgxCardModule,
-        IgxListModule,
-        IgxDialogModule,
-        IgxRippleModule,
-        IgxProgressBarModule,
-        IgxButtonGroupModule,
-        IgxSliderModule,
-        IgxRadioModule,
-        IgxTabsModule,
-        IgxDividerModule,
-        IgxSelectModule,
-        IgxSwitchModule,
-        NotificationsComponent,
-        TeamNavComponent,
-        QuickSearchComponent,
-        SearchComponent,
-        PlayerSearchComponent,
-        TeamSearchComponent,
-        GroupsFilterPipe,
-        ReduceQuickSearchResultPipe,
-        DisabledNotificationsPipe,
-        SortApplicationsPipe,
-        SortNotificationsPipe,
-        NotificationStatePipe,
-        TeamNewComponent,
-        LanguagesComponent,
-        LoginComponent,
-        SuccessErrorComponent
-      ],
-      declarations: [AppComponent]
+        AppComponent
+      ]
     }).compileComponents();
   }));
 
--- projects/bellumgens/src/main.server.ts
+++ projects/bellumgens/src/main.server.ts
@@ -1,10 +1,7 @@
-import '@angular/localize/init';
-import { enableProdMode } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { config } from './app/app.config.server';
 
-import { environment } from '../../common/src/environments/environment';
+const bootstrap = () => bootstrapApplication(AppComponent, config);
 
-if (environment.production) {
-  enableProdMode();
-}
-
-export { AppServerModule } from './app/app.server.module';
+export default bootstrap;
