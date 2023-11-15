37fdfe2eb50fc723f2a9a7b077350b436205f947
Konstantin Dinev <kdinev@mail.bw.edu>
2023-11-15 17:35:19 +0200

    chore(*): abstracting new files

---
+++ projects/bellumgens/src/app/app.routes.ts
@@ -0,0 +1,24 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TeamResultsComponent } from './search/search-results/team-results/team-results.component';
+import { PlayerResultsComponent } from './search/search-results/player-results/player-results.component';
+import { EmailconfirmComponent } from './emailconfirm/emailconfirm.component';
+import { RegistrationComponent, UnauthorizedComponent } from '../../../common/src/public_api';
+import { EventsComponent } from './events/events.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'bellumgenselite', component: EventsComponent },
+  { path: 'register', component: RegistrationComponent },
+  { path: 'unauthorized', redirectTo: 'unauthorized/', pathMatch: 'full' },
+  { path: 'unauthorized/:message', component: UnauthorizedComponent },
+  { path: 'emailconfirm', component: EmailconfirmComponent },
+  { path: 'strategies', loadChildren: () => import('./strategies/strategies.module').then(m => m.StrategiesModule) },
+  { path: 'emailconfirm/:error', component: EmailconfirmComponent },
+  { path: 'players', loadChildren: () => import('./player-section/player.module').then(m => m.PlayerModule) },
+  { path: 'team', loadChildren: () => import('./team-section/team.module').then(m => m.TeamModule) },
+  { path: 'notifications', loadChildren: () => import('./notifications/notifications.module').then(m => m.NotificationsModule) },
+  { path: 'search/teams/:query', component: TeamResultsComponent },
+  { path: 'search/players/:query', component: PlayerResultsComponent },
+  { path: '**', component: HomeComponent }
+];
