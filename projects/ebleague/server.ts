90ef3d287e65c50624da943c1200cab0a645fb27
Konstantin Dinev <kdinev@mail.bw.edu>
2023-11-16 12:02:06 +0200

    chore(ssr): refactoring server.ts-es

--- projects/ebleague/server.ts
+++ projects/ebleague/server.ts
@@ -72,14 +72,4 @@ function run(): void {
   });
 }
 
-// Webpack will replace 'require' with '__webpack_require__'
-// '__non_webpack_require__' is a proxy to Node 'require'
-// The below code is to ensure that the server is run only when not requiring the bundle.
-declare const __non_webpack_require__: NodeRequire;
-const mainModule = __non_webpack_require__.main;
-const moduleFilename = mainModule && mainModule.filename || '';
-if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
-  run();
-}
-
-export default bootstrap;
+run();
