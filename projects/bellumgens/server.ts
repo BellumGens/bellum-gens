e56f750703e8ac2e99339a143432ef22d0162d03
Konstantin Dinev <kdinev@mail.bw.edu>
2023-11-16 10:31:26 +0200

    fix(bellum-gens ssr): adjusting the server.ts

--- projects/bellumgens/server.ts
+++ projects/bellumgens/server.ts
@@ -2,19 +2,25 @@
 import 'zone.js/node';
 
 import { APP_BASE_HREF } from '@angular/common';
+import { LOCALE_ID } from '@angular/core';
 import { CommonEngine } from '@angular/ssr';
 import * as express from 'express';
 import { existsSync } from 'node:fs';
 import { join } from 'node:path';
 import bootstrap from './src/main.server';
 
+import { environment } from '../common/src/environments/environment';
+
 // The Express app is exported so that it can be used by serverless Functions.
-export function app(): express.Express {
+export function app(lang: string): express.Express {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/bellumgens/browser');
+  let distFolder = join(process.cwd(), environment.distFolderBellumGens, lang);
   const indexHtml = existsSync(join(distFolder, 'index.original.html'))
     ? join(distFolder, 'index.original.html')
     : join(distFolder, 'index.html');
+
+  const compression = require('compression');
+  server.use(compression());
 
   const commonEngine = new CommonEngine();
 
@@ -39,7 +45,9 @@ export function app(): express.Express {
         url: `${protocol}://${headers.host}${originalUrl}`,
         publicPath: distFolder,
         providers: [
-          { provide: APP_BASE_HREF, useValue: baseUrl },],
+          { provide: APP_BASE_HREF, useValue: baseUrl },
+          { provide: LOCALE_ID, useValue: lang }
+        ],
       })
       .then((html) => res.send(html))
       .catch((err) => next(err));
@@ -52,7 +60,13 @@ function run(): void {
   const port = process.env['PORT'] || 4000;
 
   // Start up the Node server
-  const server = app();
+  // Start up the Node server
+  const appBg = app('bg');
+  const appEn = app('en');
+  const server = express();
+  server.use('/bg', appBg);
+  server.use('/en', appEn);
+  server.use('', appEn);
   server.listen(port, () => {
     console.log(`Node Express server listening on http://localhost:${port}`);
   });
