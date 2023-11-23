2d4777b6d11ac4b1c6d58c78b1aafabd989d5020
Konstantin Dinev <kdinev@mail.bw.edu>
2023-11-23 14:17:08 +0200

    test(login): generating login component unit tests

---
+++ projects/common/src/environments/environment.unit.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  ebleague: 'http://localhost:9876',
+  bellumgens: 'http://localhost:9876',
+  rootApiEndpoint: 'https://localhost:44348',
+  authApiEndpoint: 'https://localhost:44348/api/account',
+  apiEndpoint: 'https://localhost:44348/api',
+  distFolderEbleague: 'dist/ebleague/browser',
+  distFolderBellumGens: 'dist/bellumgens/browser',
+  VAPID_PUBLIC_KEY: 'BNmXyw15axbl9tOAKz0ybCcfui-w4WiILSO_PRvU2qF0gdB56C0jRieaFSncZnpaulsyl0c3VUsRCyOR4foil50'
+};
