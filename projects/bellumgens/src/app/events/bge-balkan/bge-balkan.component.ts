83c6a2d85ebf1b535c58536308b6b360a839a2cd
Konstantin Dinev <kdinev@mail.bw.edu>
2024-12-02 18:47:04 +0200

    chore(*): simplifying bge-balkan component

--- projects/bellumgens/src/app/events/bge-balkan/bge-balkan.component.ts
+++ projects/bellumgens/src/app/events/bge-balkan/bge-balkan.component.ts
@@ -42,31 +42,31 @@ export class BgeBalkanComponent extends BaseDirective {
               route: ActivatedRoute) {
     super(title, meta, route);
     this.loginService.applicationUser.subscribe(user => this.authUser = user);
-
-    this.activeRoute.params.subscribe(params => {
-      this.tournamentId = params['tournamentid'];
-      if (this.tournamentId) {
-        this.apiService.getTournament(this.tournamentId).subscribe(t => this.tournament = t);
+    this.apiService.activeTournament.subscribe(t => {
+      if (t) {
+        this.tournament = t;
+        this.tournamentId = t.id;
         this.apiService.loadingSC2Registrations.subscribe(data => this.loading = data);
         this.apiService.getSc2Registrations(this.tournamentId).subscribe(data => this.registrations = data);
         this.apiService.loadingSC2Matches.subscribe(data => this.loadingMatches = data);
-        this.apiService.getSc2Matches(this.tournamentId).subscribe(data => this.sc2matches = data);
+        this.apiService.getSc2Matches(this.tournamentId).subscribe(matches => this.sc2matches = matches);
         this.apiService.loadingSC2Groups.subscribe(data => this.loadingGroups = data);
         this.apiService.getSc2Groups(this.tournamentId).subscribe(data => this.groups = data);
-      } else {
-        this.apiService.activeTournament.subscribe(t => {
-          if (t) {
-            this.tournament = t;
-            this.tournamentId = t.id;
-            this.apiService.loadingSC2Registrations.subscribe(data => this.loading = data);
-            this.apiService.getSc2Registrations(this.tournamentId).subscribe(data => this.registrations = data);
-            this.apiService.loadingSC2Matches.subscribe(data => this.loadingMatches = data);
-            this.apiService.getSc2Matches(this.tournamentId).subscribe(matches => this.sc2matches = matches);
-            this.apiService.loadingSC2Groups.subscribe(data => this.loadingGroups = data);
-            this.apiService.getSc2Groups(this.tournamentId).subscribe(data => this.groups = data);
-          }
-        });
       }
     });
+
+    // this.activeRoute.params.subscribe(params => {
+    //   this.tournamentId = params['tournamentid'];
+    //   if (this.tournamentId) {
+    //     this.apiService.getTournament(this.tournamentId).subscribe(t => this.tournament = t);
+    //     this.apiService.loadingSC2Registrations.subscribe(data => this.loading = data);
+    //     this.apiService.getSc2Registrations(this.tournamentId).subscribe(data => this.registrations = data);
+    //     this.apiService.loadingSC2Matches.subscribe(data => this.loadingMatches = data);
+    //     this.apiService.getSc2Matches(this.tournamentId).subscribe(data => this.sc2matches = data);
+    //     this.apiService.loadingSC2Groups.subscribe(data => this.loadingGroups = data);
+    //     this.apiService.getSc2Groups(this.tournamentId).subscribe(data => this.groups = data);
+    //   } else {
+    //   }
+    // });
   }
 }
