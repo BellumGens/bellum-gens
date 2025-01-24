f1d410e86209525e6df036a296cf44de3b4af63a
Konstantin Dinev <kdinev@mail.bw.edu>
2025-01-24 12:17:36 +0200

    chore: tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,6 +2,6 @@ describe('My First Test', () => {
   it('Visits the initial project page', () => {
     cy.visit('/')
     cy.contains('Bellum Gens')
-    cy.contains('This site uses cookies.')
+    cy.contains('Esports Events')
   })
 })
