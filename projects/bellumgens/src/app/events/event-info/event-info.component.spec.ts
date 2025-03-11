66da6554ef70ab8a14ceb8da4cbc61f18ff68db2
Konstantin Dinev <kdinev@mail.bw.edu>
2025-03-11 16:27:40 +0200

    chore(*): adding tests and fixing them

--- projects/bellumgens/src/app/events/event-info/event-info.component.spec.ts
+++ projects/bellumgens/src/app/events/event-info/event-info.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EventInfoComponent } from './event-info.component';
 import { ApiTournamentsService } from '../../../../../common/src/public_api';
-import { BehaviorSubject, Observable, of } from 'rxjs';
+import { BehaviorSubject, Observable, of, take } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { Title, Meta } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('EventInfoComponent', () => {
   let component: EventInfoComponent;
   let fixture: ComponentFixture<EventInfoComponent>;
   let apiService: ApiTournamentsService;
-  let getTSpy: jasmine.Spy;
-  let getSc2Spy: jasmine.Spy;
+  //let loginService: LoginService;
+  let httpMock: HttpTestingController;
   let metaService: Meta;
 
   beforeEach(async () => {
@@ -32,44 +32,63 @@ describe('EventInfoComponent', () => {
       ]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(EventInfoComponent);
-    component = fixture.componentInstance;
     apiService = TestBed.inject(ApiTournamentsService);
     metaService = TestBed.inject(Meta);
-
-    getTSpy = spyOn(apiService, "getTournament").and.returnValue(new BehaviorSubject({
-      id: '123',  name: 'Test Tournament', description: 'Test Description', logo: 'test-logo'
-    }));
-    getSc2Spy = spyOn(apiService, "getSc2Registrations").and.returnValue(new BehaviorSubject([
-      { id: '123', userId: '123', state: 0, companyId: 'company', tournamentId: '123' },
-      { id: '124', userId: '124', state: 1, companyId: 'company 2', tournamentId: '123' }
-    ]));
-
-    fixture.detectChanges();
+    httpMock = TestBed.inject(HttpTestingController);
   });
+
+    beforeEach(() => {
+      fixture = TestBed.createComponent(EventInfoComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
   it('should set tournament details on init', () => {
-    expect(component.tournament.name).toBe('Test Tournament');
-    expect(component.tournament.description).toBe('Test Description');
-  });
-
-  it('should call getSc2Registrations and getSc2Matches on init', () => {
-    expect(getTSpy).toHaveBeenCalledWith('123');
-    expect(getSc2Spy).toHaveBeenCalledWith('123');
-  });
-
-  it('should update meta tags on init', () => {
+    const mockTournament = { id: '123', name: 'Test Tournament', description: 'Test Description', logo: 'test-logo' } as any;
+    let req = httpMock.expectOne(`${apiService['_apiEndpoint']}/tournament?id=123`);
+    expect(req.request.method).toBe('GET');
+    expect(req.request.withCredentials).toBeFalse();
+    req.flush(mockTournament);
+    expect(component.tournament).toEqual(mockTournament);
+    expect(component.tournamentId).toBe('123');
     expect(metaService.getTag('name="og:title"').content).toBe('Test Tournament');
     expect(metaService.getTag('name="og:image"').content).toBe('test-logo');
     expect(metaService.getTag('name="description"').content).toBe('Test Description');
     expect(metaService.getTag('name="twitter:title"').content).toBe('Test Tournament');
   });
 
+  it('should call getSc2Registrations and getSc2Matches on init', () => {
+    const mockRegistrations = [{ id: 'reg1' }] as any[];
+    const mockTournament = { id: '123', name: 'Test Tournament', description: 'Test Description', logo: 'test-logo' } as any;
+    let req = httpMock.expectOne(`${apiService['_apiEndpoint']}/tournament?id=123`);
+    expect(req.request.method).toBe('GET');
+    expect(req.request.withCredentials).toBeFalse();
+    req.flush(mockTournament);
+
+    component.loading.pipe(take(1)).subscribe(value => expect(value).toBeTrue());
+    req = httpMock.expectOne(`${apiService['_apiEndpoint']}/tournament/sc2regs?tournamentId=123`);
+    expect(req.request.method).toBe('GET');
+    expect(req.request.withCredentials).toBeFalse();
+    req.flush(mockRegistrations);
+    component.registrations.pipe(take(1)).subscribe(value => expect(value).toEqual(mockRegistrations));
+    component.loading.pipe(take(1)).subscribe(value => expect(value).toBeFalse());
+
+    const mockMatches = [{ id: 'match1' }] as any[];
+    component.loadingMatches.pipe(take(1)).subscribe(value => expect(value).toBeTrue());
+    req = httpMock.expectOne(`${apiService['_apiEndpoint']}/tournament/sc2matches?tournamentId=123`);
+    expect(req.request.method).toBe('GET');
+    expect(req.request.withCredentials).toBeFalse();
+    req.flush(mockMatches);
+    component.loadingMatches.pipe(take(1)).subscribe(value => expect(value).toBeFalse());
+    component.sc2matches.pipe(take(1)).subscribe(value => expect(value).toEqual(mockMatches));
+  });
+
   it('should refresh matches', () => {
+    const spy = spyOn(apiService, 'getSc2Matches');
     component.refreshMatches();
     expect(apiService.getSc2Matches).toHaveBeenCalledWith('123');
   });
