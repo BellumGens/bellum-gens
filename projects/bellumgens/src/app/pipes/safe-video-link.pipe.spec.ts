244e7716f08c4d8ae75e1c4ed6ebce32594da5a2
Konstantin Dinev <kdinev@mail.bw.edu>
2025-06-23 13:23:36 +0300

    chore(*): fixing lint

--- projects/bellumgens/src/app/pipes/safe-video-link.pipe.spec.ts
+++ projects/bellumgens/src/app/pipes/safe-video-link.pipe.spec.ts
@@ -1,6 +1,6 @@
 import { SafeVideoLinkPipe } from './safe-video-link.pipe';
 import { TestBed, inject } from '@angular/core/testing';
-import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer } from '@angular/platform-browser';
 
 describe('SafeVideoLinkPipe', () => {
   beforeEach(() => {
