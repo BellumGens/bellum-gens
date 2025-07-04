83628532e9f59cc4a1c4cba466c84fb6fc5ff5cc
Konstantin Dinev <kdinev@mail.bw.edu>
2025-07-04 14:46:52 +0300

    chore(*): removing unused imports

--- projects/ebleague/src/app/shop/shop.component.ts
+++ projects/ebleague/src/app/shop/shop.component.ts
@@ -1,10 +1,8 @@
 import { Component, inject, OnInit, ViewChild } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { NgOptimizedImage } from '@angular/common';
 
-import { EMPTY_JERSEY_ORDER, JerseyCut, JerseyDetails, JerseySize, ApiShopService, OrderformComponent } from '../../../../common/src/public_api';
+import { OrderformComponent } from '../../../../common/src/public_api';
 import { Router } from '@angular/router';
-import { ISelectionEventArgs, IGX_SELECT_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IgxIconComponent, IgxDividerDirective, IgxButtonDirective, IgxMaskDirective, IgxTextSelectionDirective } from '@infragistics/igniteui-angular';
 import { BaseDirective } from '../../../../bellumgens/src/app/base/base.component';
 
 @Component({
